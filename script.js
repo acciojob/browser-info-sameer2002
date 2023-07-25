7c5260ca934c8c2634c602df025175e6ab298669
sameer sagar <60567209+sameer2002@users.noreply.github.com>
2023-07-25 15:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const browserInfo = document.getElementById('browser-info');
+const text = "You are using "+ navigator.userAgent+ " version "+navigator.appVersion;
+browserInfo.innerText = text;
